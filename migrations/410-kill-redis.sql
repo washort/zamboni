c4bfaa554258c5f3307e1032e77adfa4fda71332
Allen Short <ashort@mozilla.com>
2012-05-18 22:25:49 -0500

    Replace redis with db table for FakeEmailBackend

--- migrations/410-kill-redis.sql
+++ migrations/410-kill-redis.sql
@@ -1,4 +1,11 @@
 CREATE TABLE `compat_totals` (
        `app` integer NOT NULL PRIMARY KEY,
        `totals` integer
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `fake_email` (
+       `id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY,
+       `message` text NOT NULL,
+       `created` datetime NOT NULL default '0000-00-00 00:00:00',
+       `modified` datetime NOT NULL default '0000-00-00 00:00:00',
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
